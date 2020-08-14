32fad3dbe295f2c50932a754384b8cf2eb87be1e
Simon Reeves <simon.reeves@mnscorp.net>
2020-08-14 17:20:55 +0100

    Added GitRetrieval test

--- src/main/java/com/marksandspencer/test/GitRetrieval.java
+++ src/main/java/com/marksandspencer/test/GitRetrieval.java
@@ -1,7 +1,15 @@
 package com.marksandspencer.test;
 
+import java.io.IOException;
+import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,16 +20,58 @@ public class GitRetrieval implements Runnable{
 
   private final RestTemplate restTemplate;
 
-  public GitRetrieval(RestTemplate restTemplate){
+  private final String gitToken;
+  private final String repository;
+  private final String gitUser;
+  private final String gitBranch;
+  private final String gitPath;
+
+  public GitRetrieval(RestTemplate restTemplate,
+      @Value("${git.token}")
+      String gitToken,
+      @Value("${git.repository}")
+      String gitRepo,
+      @Value("${git.user}")
+      String gitUser,
+      @Value("${git.branch}")
+      String gitBranch,
+      @Value("${git.path}")
+      String gitPath){
     this.restTemplate = restTemplate;
 
+    this.gitToken = gitToken;
+    this.repository = gitRepo;
+    this.gitUser = gitUser;
+    this.gitBranch = gitBranch;
+    this.gitPath = gitPath;
   }
 
   public void run(){
 
     LOGGER.info("GitRetrieval starting");
 
+    try {
 
+      // Try Personal Access Token(PAT)
+      GitHub gitHub = new GitHubBuilder().withOAuthToken(gitToken, gitUser).build();
+
+      GHRepository repo = gitHub.getRepository(repository);
+
+      LOGGER.info("Got GHRepo : {}", repo);
+
+      GHBranch ghBranch = repo.getBranch(gitBranch);
+
+      LOGGER.info("Got GHBrnach : {}", ghBranch);
+
+      GHContent fileContents = repo.getFileContent(gitPath);
+
+      String content = new String(fileContents.read().readAllBytes());
+
+      LOGGER.info(content);
+
+    } catch (IOException e) {
+      LOGGER.error("Exception ",e);
+    }
 
     LOGGER.info("GitRetrieval ends");
 
